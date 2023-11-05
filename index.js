80ec970711f6d71678c756f591d809a1629ac9e8
Keelan <91790708+keelanmc1@users.noreply.github.com>
2023-11-05 18:48:33 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const express = require('express'); 
-const app = express(); 
-require('dotenv/config'); 
-const mongoose = require('mongoose'); 
-const user = require('./routes/Users'); 
-const bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+require("dotenv/config");
+const mongoose = require("mongoose");
+const user = require("./routes/Users");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
+
 // Middleware
-app.use(bodyParser.json()); 
-app.use('/users', user); 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use("/users", user);
 
 // Database connection
-try{
-    mongoose.connect(process.env.DB_CONNECTION, () => console.log('connected to database')); 
-} catch (err){
-    console.log (`Unable to connect to database with following error: ${err}`); 
+try {
+  mongoose.connect(process.env.DB_CONNECTION, () =>
+    console.log("connected to database")
+  );
+} catch (err) {
+  console.log(`Unable to connect to database with following error: ${err}`);
 }
 
-// home route.
-app.get('/', (req, res) => {
-    res.send("Hello, this is the home page!"); 
-}); 
+app.get("/", (req, res) => {
+  res.send("Hello, this is the home page!");
+});
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
